a19b85f4e4768386cab4c5dc174ec8ba5a0b4ad3
Eranmonnie <ajalaferanmi123@gmail.com>
2023-08-02 09:34:04 +0100

    linear search

---
+++ linear_search/linear_search.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int linear_search(int* arr , int val);
+
+int main(){
+
+    int arr [] = {1,3,5,7,2,9};
+    cout<<linear_search(arr, 2);
+
+return 0;
+}
+
+int linear_search(int* arr, int val){
+
+    int length = sizeof(arr);
+
+    for (int i=0; i<length; i++){
+        if (arr[i] == val){
+            return i;
+        }    
+    }
+    
+    
+}
